85e7429ee336041cea7bc465accb50780e9ba7fb
ncastellanosort <nicolasfelipe00234@gmail.com>
2026-02-02 11:37:11 -0500

    docs: cap5 v2

--- src/cap5_structs.rs
+++ src/cap5_structs.rs
@@ -4,8 +4,33 @@ pub struct Usuario {
     pub activo: bool
 }
 
-// para poder imprimirlo bien
-// es un atributo
+
+// --------------------- TUPLE STRUCTS  ----------------------
+/* 
+ * se usan cuando se quiere que la struct tenga un nombre (para que sea un tipo diferente a los
+ * otros) pero por el momento los nombres de los campos no son necesarios
+*/
+
+pub struct Color(i32, i32, i32);
+
+fn ejemplo_tuple_structs() {
+    let negro_rgb = Color(0,0,0);
+    println!("valor del negro en el verde es: {}", negro_rgb.0);
+}
+
+// --------------------- UNIT-LIKE STRUCTS  ----------------------
+/* 
+ * fundamentales para el uso de los traits (CAP. 10)
+ * se usan cuando se necesita implementar un comportamiento en algun tipo, pero no se necesita que
+ * ese tipo guarde ningun dato
+ *
+ * "este objeto solo existe para poder llamar a sus funciones"
+*/
+
+struct SiempreIgual;
+
+
+// es un atributo para poder imprimir bien el struct
 #[derive(Debug)]
 pub struct Rectangulo {
     pub ancho: i32,
@@ -36,10 +61,12 @@ pub fn struct_ejemplo() {
         activo: true
     };
 
+    // setear atributos
     u1.activo = false;
 
     println!("usuario: {:?}", u1);
 
+    // uso de la funcion asociada
     let mut r1 = Rectangulo::new(10, 40);
     r1.set_ancho(5);
 
